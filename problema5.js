3bc3e65e28dd39daabddb5e4a69504a2482974e0
Anibal Rivero <riveroleonel@gmail.com>
2023-04-25 14:54:44 -0300

    problema 5 parte 1

--- problema5.js
+++ problema5.js
@@ -1,19 +1,24 @@
 const CANT_MAX_MASCOTAS = 3;
 const CANT_MAX_GATOS = 1;
 const CANT_MAX_PERROS = CANT_MAX_MASCOTAS;
-const CANT_POR_DEF = 0;
+const CANT_MASCOTAS_DEF = 0;
 
 let consultaMascota = prompt("Quiere pasear perros o un gato");
 let numDeMascotas = prompt("Ingrese la cantidad de mascotas");
 let opcionUno = "perros";
 let opcionDos = "gato";
 
-if ((consultaMascota == opcionUno) && (numDeMascotas == CANT_MAX_PERROS)) {
+if (((consultaMascota == opcionUno) && (numDeMascotas <= CANT_MAX_PERROS) && (numDeMascotas > CANT_MASCOTAS_DEF))) {
     document.write("Pedido confirmado : el paseador llegara en 20 minutos");
-} else if ((consultaMascota == opcionDos) && (numDeMascotas == CANT_MAX_GATOS)) {
+} else if (((consultaMascota == opcionDos) && (numDeMascotas == CANT_MAX_GATOS) && (numDeMascotas > CANT_MASCOTAS_DEF))) {
     document.write("Pedido confirmado : el paseador llegara en 20 minutos");
-} else if (numDeMascotas == CANT_POR_DEF) {
-    document.write("Numero invalido")
-}else {
-    document.write("Limite de mascotas excedido intente ingresar otro numero");
+} 
+
+else {
+    document.write("Numero incorrecto de mascotas : intente nuevamente");
 }
+
+
+
+
+
